f46ee2af725e01aae0ed80cc24b875ee09349e06
Samantha Gatt <smfgatt@gmail.com>
2018-08-08 15:03:31 -0400

    Configures message thread detail view

--- Message Board/Message Board/VCs/MessageThreadDetailTableViewController.swift
+++ Message Board/Message Board/VCs/MessageThreadDetailTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MessageThreadsTableViewController.swift
+//  MessageThreadDetailTableViewController.swift
 //  Message Board
 //
 //  Created by Samantha Gatt on 8/8/18.
@@ -8,51 +8,37 @@
 
 import UIKit
 
-class MessageThreadsTableViewController: UITableViewController {
+class MessageThreadDetailTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         
     }
 
 
     // MARK: - Table view data source
 
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageThreadCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        
+        // Configure the cell...
 
         return cell
     }
-    
-    
-    // MARK: - Actions
-    
-    @IBAction func submitMessageThread(_ sender: Any) {
-    }
-    
-    
-    
-    // MARK: - Outlets
-    
-    @IBOutlet weak var textField: UITextField!
-    
-    
-    
-    // MARK: - Properties
-    
-    
-    
+
     
     // MARK: - Navigation
-    
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let segueID = "ShowMessageThreadDetails"
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
     }
+
 }
